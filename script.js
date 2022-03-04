5d9374a0b332e9dbb70b8e3a49168cd1a5273cb4
Ilton.pjj@gmail.com <Ilton.pjj@gmail.com>
2022-03-04 20:54:53 -0300

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+on_off = true
+function openNav() {
+  if(on_off){
+    document.querySelector(".NavList").style.display = "block"
+    Icons = document.querySelectorAll(".NavBarsIcon")
+    Icons[1].style.position = "absolute"
+    Icons[1].style.transform = "rotate(45deg)"
+    Icons[2].style.transform = "rotate(135deg)"
+    Icons[1].style.top = "10vw"
+    Icons[1].style.background = "#0fbaaa"
+    Icons[2].style.background = "#0fbaaa"
+    on_off = false
+    
+
+  }
+  else {
+    document.querySelector(".NavList").style.display = "none"
+    Icons = document.querySelectorAll(".NavBarsIcon")
+    Icons[1].style.transform = "rotate(180deg)"
+    Icons[1].style.position = "static"
+    Icons[2].style.transform = "rotate(180deg)"
+    Icons[1].style.background = "#fff"
+    Icons[2].style.background = "#fff"
+    on_off = true
+
+  }
+}
